694df91e3782e35b40528430a4db12bee6d8e3a7
awnlzw <awnlzw@126.com>
2024-07-10 14:44:50 +0800

    fix: hm module error

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -43,7 +43,7 @@ in {
       };
       Service = {
         Type = "simple";
-        ExecStart = "${cfg.package}/bin/clipshare --port ${lib.toString cfg.port}";
+        ExecStart = "${cfg.package}/bin/clipshare --port ${toString cfg.port}";
         Restart = "on-failure";
       };
       Install.WantedBy = [
