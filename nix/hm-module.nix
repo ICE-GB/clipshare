a7866371edb049fb0ea6f3ca3933a44e24b25e6d
awnlzw <awnlzw@126.com>
2024-09-26 16:53:22 +0800

    feat: add environmentFile option

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -1,13 +1,16 @@
-self: {
+self:
+{
   config,
   pkgs,
   lib,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.programs.clipshare;
   defaultPackage = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
-in {
+in
+{
   options.programs.clipshare = with types; {
     enable = mkEnableOption "Whether or not to enable clipshare.";
     package = mkOption {
@@ -33,15 +36,26 @@ in {
         clipshare server port
       '';
     };
+    environmentFile = lib.mkOption {
+      description = ''
+        Environment file to be passed to the systemd service.
+        Useful for passing secrets to the service to prevent them from being
+        world-readable in the Nix store.
+      '';
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      example = "/var/lib/secrets/clipshareSecrets";
+    };
   };
 
   config = mkIf cfg.enable {
     systemd.user.services.clipshare = lib.mkIf cfg.systemd {
       Unit = {
         Description = "Systemd service for Clipshare";
-        Requires = ["graphical-session.target"];
+        Requires = [ "graphical-session.target" ];
       };
       Service = {
+        EnvironmentFile = lib.optional (cfg.environmentFile != null) cfg.environmentFile;
         Type = "simple";
         ExecStart = "${cfg.package}/bin/clipshare --port ${toString cfg.port}";
         Restart = "on-failure";
